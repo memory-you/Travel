17d01b37bebb727b669fdd3daf935a5f7c618eb2
memory-you <869529706@qq.com>
2019-04-17 14:32:05 +0800

    detail banner

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="banner">
+    <img
+      class="banner-img"
+      src="//img1.qunarzz.com/sight/p0/1604/1e/1e75f8e1af9fe81f90.img.jpg_600x330_aeba2c96.jpg"
+      alt=""
+    >
+    <div class="banner-info">
+      <div>大连圣亚海洋世界(AAAA景区)</div>
+      <div>39</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+.banner
+  overflow hidden
+  height 0
+  padding-bottom 55%
+  .banner-img
+    width 100%
+</style>
